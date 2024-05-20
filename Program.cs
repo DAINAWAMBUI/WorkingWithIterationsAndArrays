1d4bbf348f4285bb2933877baab382bd1c0f1dcd
Daina Muiruri <61407935+DAINAWAMBUI@users.noreply.github.com>
2024-05-20 15:18:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+ 
+namespace forIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z;
+            for (z = 0; z < 50; z++)
+            {
+               // Console.WriteLine(z.ToString());
+            }
+
+            for (int myValue = 0; myValue < 30; myValue++ )
+            {
+                Console.WriteLine(myValue);
+            }
+               Console.ReadLine();
+            
+        }
+    }
+}
